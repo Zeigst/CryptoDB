76dc31c3c0e1a9aae554ba173c59dfa0949b471f
Zeigst <94588594+Zeigst@users.noreply.github.com>
2022-12-15 20:22:36 +0700

    src ver 3

---
+++ src/main/java/com/example/myapplication/CryptoCoin.java
@@ -0,0 +1,62 @@
+package com.example.myapplication;
+
+public class CryptoCoin implements Comparable<CryptoCoin>{
+    private String name;
+    private float price;
+    private float change;
+    private float cap;
+    private float supply;
+
+    public CryptoCoin(String name, float price, float change, float cap, float supply) {
+        this.name = name;
+        this.price = price;
+        this.change = change;
+        this.cap = cap;
+        this.supply = supply;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getChange() {
+        return change;
+    }
+
+    public void setChange(float change) {
+        this.change = change;
+    }
+
+    public float getCap() {
+        return cap;
+    }
+
+    public void setCap(float cap) {
+        this.cap = cap;
+    }
+
+    public float getSupply() {
+        return supply;
+    }
+
+    public void setSupply(float supply) {
+        this.supply = supply;
+    }
+
+    @Override
+    public int compareTo(CryptoCoin cryptoCoin) {
+        return 0;
+    }
+}
